89c5c34c4d316a0c513c65c218e5a298893577f8
swagy_.pravi <kpravin300906@gmail.com>
2026-02-06 12:42:51 +0530

    Create hollow diamond pattern.c

---
+++ hollow diamond pattern.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() {
+    int n , i, j;
+    scanf("%d",&n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = i; j < n; j++)
+            printf(" ");
+
+        for (j = 1; j <= (2*i - 1); j++) {
+            if (j == 1 || j == (2*i - 1))
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    for (i = n-1; i >= 1; i--) {
+        for (j = n; j > i; j--)
+            printf(" ");
+
+        for (j = 1; j <= (2*i - 1); j++) {
+            if (j == 1 || j == (2*i - 1))
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
