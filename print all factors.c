1d1c11207fd3e2657080e428fb30058d8dbbc073
swagy_.pravi <kpravin300906@gmail.com>
2026-02-06 11:03:13 +0530

    Create print all factors.c

---
+++ print all factors.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a,i;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++){
+        if(a%i==0)
+        printf("%d ",i);
+    }
+        return 0;
+}
+
+
