9ac6087afd3f5face2bbf709933759f94ca8f35b
swagy_.pravi <kpravin300906@gmail.com>
2026-02-06 12:28:49 +0530

    Create reverse the number.c

---
+++ reverse the number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int num,rev=0,last;
+	scanf("%d",&num);
+	while(num!=0){
+	    last=num%10;
+	    rev=rev*10+last;
+	    num=num/10;
+	}
+	printf("%d",rev);
+	return 0;
+}
