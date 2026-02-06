0fc9625f325051cb77327d280c56c9cfbdc79ec6
swagy_.pravi <kpravin300906@gmail.com>
2026-02-06 12:37:21 +0530

    Create rectangle pattern.c

---
+++ rectangle pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a,b;
+    scanf("%d%d",&a,&b);
+    for(int i=1;i<=a;i++){
+        for(int j=1;j<=b;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
